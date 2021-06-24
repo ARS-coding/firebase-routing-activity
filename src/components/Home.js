003bf1a8b0d944f128438edacebdd9451fea1107
halit <halitfuatbatur@gmail.com>
2021-06-24 18:30:27 +0300

    add comment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,5 @@
 import React from "react";
+// You will be able to use this, when you paste your firestore configs inside the firebaseConfig.js file
 import db from "../firebaseConfig";
 
 export const Home = () => {
