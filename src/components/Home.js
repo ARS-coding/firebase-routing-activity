fb35eab9a343cc8cac6065dc85c5e543e26814b3
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-04 17:10:03 +0300

    Install the npm dependencies and add the scripts in index.html to see the componens in react browser dev tools.

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,8 @@ import React from "react";
 // You will be able to use this, when you paste your firestore configs inside the firebaseConfig.js file
 import db from "../firebaseConfig";
 
-export const Home = () => {
-  return <div></div>;
+const Home = () => {
+  return <div><h1>Home</h1></div>;
 };
+
+export default Home;
--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,7 +2,13 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 const firebaseConfig = {
-  //  Add your own configs
+  apiKey: "AIzaSyDBwM7LG1GPeu18Xb_3kx6ZA4IpU0kaPe0",
+  authDomain: "fir-routing-activity-35a5d.firebaseapp.com",
+  projectId: "fir-routing-activity-35a5d",
+  storageBucket: "fir-routing-activity-35a5d.appspot.com",
+  messagingSenderId: "220913373782",
+  appId: "1:220913373782:web:c045658538e4a47182e1f7",
+  measurementId: "G-GVGKQ5MK1F"
 };
 
 firebase.initializeApp(firebaseConfig);
