21fca70b7d084dc0162781e6f45156c80eb6344c
Ali Rıza <ars.lifestyle@hotmail.com>
2021-07-04 17:43:58 +0300

    Make the form controlled.

--- src/components/HomeForm.js
+++ src/components/HomeForm.js
@@ -1,13 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 function HomeForm() {
+
+    const [formData, setFormData] = useState({
+        username: "",
+        email: "",
+        password: ""
+    });
+
+    function handleChange(event) {
+        setFormData({...formData, [event.target.name]: event.target.value});
+    };
+
     return (
         <div className="home-form-wrapper">
             <form>
-                <input type="text" placeholder="Username..." />
-                <input type="Email" placeholder="Email..." />
-                <input type="password" placeholder="Password..." />
-                <input type="submit" value="Create Account" />
+                <input 
+                    type="text"
+                    name="username"
+                    onChange={handleChange} 
+                    value={formData.username} 
+                    placeholder="Username..."
+                />
+                <input 
+                    type="Email"
+                    name="email"
+                    onChange={handleChange}
+                    value={formData.email}
+                    placeholder="Email..."
+                />
+                <input 
+                    type="password" 
+                    name="password" 
+                    onChange={handleChange}
+                    value={formData.password}
+                    placeholder="Password..." 
+                />
+                <input
+                    type="submit"
+                    onChange={handleChange}
+                    value="Create Account"
+                />
             </form>
         </div>
     )
